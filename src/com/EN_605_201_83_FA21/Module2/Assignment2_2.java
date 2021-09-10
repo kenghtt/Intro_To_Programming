f817479e88fa74d80fde25318d67c92f4e01bff7
jeremytouch <kenghtt@gmail.com>
2021-09-10 15:32:14 -0500

    Assignment2 updates

--- src/com/EN_605_201_83_FA21/Module2/Assignment2_2.java
+++ src/com/EN_605_201_83_FA21/Module2/Assignment2_2.java
@@ -10,13 +10,14 @@ import java.util.Scanner;
 public class Assignment2_2 {
     public static void main(String[] args) {
         // Define and initialize variables for values to be input
-        double weightInPounds = 0;      // First value to be input
-        double heightInInches = 0;      // Second value to be input
+        double weightInPounds = 0;
+        double heightInInches = 0;
+
         double weightInKilos = 0;
         double heightInMeters = 0;
         double bmi = 0;
 
-        // Use a Scanner to input integer values
+        // Use a Scanner to input values
         Scanner input = new Scanner(System.in);
         System.out.println("\n");
         System.out.print("Enter a weight by pounds:");
@@ -25,24 +26,19 @@ public class Assignment2_2 {
         heightInInches = input.nextInt();     // Input second value
 
         // Given that one pound is 0.45359237 kilograms and one inch is 0.0254 meters.
-//        Formula: 703 x weight (lbs) / [height (in)]2
-
         weightInKilos = weightInPounds * 0.45359237;
         heightInMeters = heightInInches * 0.0254;
-        bmi = weightInKilos / (heightInMeters * heightInMeters);
 
-        System.out.println("\n");
+        bmi = weightInKilos / (heightInMeters * heightInMeters); //
 
-        System.out.println("Your BMI is: "+ bmi);
-
-
-
+        System.out.println("");
         // Output using System.out.println()
-        System.out.println("\n");
-        System.out.println("Underweight: less than 18.5 ");
-        System.out.println("Normal: 18.5 – 24.9 ");
-        System.out.println("Overweight: 25 – 29.9 ");
-        System.out.println("Obese: 30 or greater ");
-        System.out.println("\n\n");
+        System.out.println("Your BMI is: "+ bmi);
+        System.out.println("");
+        System.out.println("\tUnderweight: less than 18.5 ");
+        System.out.println("\tNormal: 18.5 – 24.9 ");
+        System.out.println("\tOverweight: 25 – 29.9 ");
+        System.out.println("\tObese: 30 or greater ");
+        System.out.println("");
     }
 }
